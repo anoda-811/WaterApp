a5ef36c6d9cb487ef620b705a54be82e3cbb5c33
anoda <atusiufo@yahoo.co.jp>
2026-02-21 14:49:20 +0900

    nine-2

---
+++ app/nine/time-attack/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function TimeAttack() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      Time Attack
+    </div>
+  );
+}
